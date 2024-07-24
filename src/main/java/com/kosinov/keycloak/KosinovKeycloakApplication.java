3b6556babcd5fdfff0ba2bbba1cf504c16f42b52
kosinovv <kosinov_va@mail.ru>
2024-07-24 10:01:15 +0300

    Первичная фиксация проекта

---
+++ src/main/java/com/kosinov/keycloak/KosinovKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.kosinov.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosinovKeycloakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KosinovKeycloakApplication.class, args);
+    }
+
+}
